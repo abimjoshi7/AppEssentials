0c9ff7903ab8a647bdc511bb8f661d423004dfe4
abimjoshi7 <abimjoshi7@gmail.com>
2025-01-31 12:57:05 +0545

    feat: made exception base for all generics

--- utils/lib/generics/base_datasource.dart
+++ utils/lib/generics/base_datasource.dart
@@ -3,17 +3,17 @@ import 'dart:async';
 import 'package:utils/utils.dart';
 
 abstract class RemoteDataSource<T> {
-  FutureOr<Either<AppException, List<T>>> fetchAll({int? page, int? pageSize});
-  FutureOr<Either<AppException, T>> fetchById(String id);
-  FutureOr<Either<AppException, T>> create(T item);
-  FutureOr<Either<AppException, T>> update(String id, T item);
-  FutureOr<Either<AppException, None>> delete(String id);
+  FutureOr<Either<Exception, List<T>>> fetchAll({int? page, int? pageSize});
+  FutureOr<Either<Exception, T>> fetchById(String id);
+  FutureOr<Either<Exception, T>> create(T item);
+  FutureOr<Either<Exception, T>> update(String id, T item);
+  FutureOr<Either<Exception, None>> delete(String id);
 }
 
 abstract class LocalDataSource<T> {
-  FutureOr<Either<AppException, List<T>>> fetchAll();
-  FutureOr<Either<AppException, T?>> fetchById(String id);
-  FutureOr<Either<AppException, int>> save(T item);
-  FutureOr<Either<AppException, int>> delete(String id);
-  FutureOr<Either<AppException, int>> clearCache();
+  FutureOr<Either<Exception, List<T>>> fetchAll();
+  FutureOr<Either<Exception, T?>> fetchById(String id);
+  FutureOr<Either<Exception, int>> save(T item);
+  FutureOr<Either<Exception, int>> delete(String id);
+  FutureOr<Either<Exception, int>> clearCache();
 }
--- utils/lib/generics/base_repository.dart
+++ utils/lib/generics/base_repository.dart
@@ -4,17 +4,17 @@ import 'package:utils/utils.dart';
 
 abstract class BaseRepository<T> {
   /// Fetch a list of items, optionally paginated.
-  FutureOr<Either<AppException, List<T>>> fetchAll({int? page, int? pageSize});
+  FutureOr<Either<Exception, List<T>>> fetchAll({int? page, int? pageSize});
 
   /// Fetch a single item by its ID.
-  FutureOr<Either<AppException, T>> fetchById(String id);
+  FutureOr<Either<Exception, T>> fetchById(String id);
 
   /// Create a new item.
-  FutureOr<Either<AppException, T>> create(T item);
+  FutureOr<Either<Exception, T>> create(T item);
 
   /// Update an existing item.
-  FutureOr<Either<AppException, T>> update(String id, T item);
+  FutureOr<Either<Exception, T>> update(String id, T item);
 
   /// Delete an item by its ID.
-  FutureOr<Either<AppException, void>> delete(String id);
+  FutureOr<Either<Exception, void>> delete(String id);
 }
