6adeb2e74858fada1288d40e1dcf79cd356bbc76
abimjoshi7 <abimjoshi7@gmail.com>
2025-03-05 15:56:52 +0545

    feat: custom search refractor

--- widgets/lib/src/custom_search.dart
+++ widgets/lib/src/custom_search.dart
@@ -1,19 +1,33 @@
 part of 'src.dart';
 
 class CustomSearch<T> extends SearchDelegate<T> {
-  final List<T> list;
+  final Future<List<T>> Function(String query) fetchResults;
   final String Function(T t) getTitle;
   final ValueChanged<String?>? onQuery;
   final ValueChanged<T>? onResult;
   final bool hasScanner;
 
+  Timer? _debounce;
+  List<T> _results = [];
+
   CustomSearch({
+    required this.fetchResults,
     required this.getTitle,
     this.onQuery,
     this.onResult,
-    this.list = const [],
     this.hasScanner = false,
   });
+
+  void _search(BuildContext context, String query) {
+    onQuery?.call(query);
+
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () async {
+      final data = await fetchResults(query);
+      _results = data;
+      if (context.mounted) showSuggestions(context); // Refresh UI
+    });
+  }
 
   @override
   List<Widget>? buildActions(BuildContext context) {
@@ -21,9 +35,16 @@ class CustomSearch<T> extends SearchDelegate<T> {
       if (hasScanner)
         IconButton(
           onPressed: () {},
-          icon: const Icon(
-            Icons.qr_code_scanner_rounded,
-          ),
+          icon: const Icon(Icons.qr_code_scanner_rounded),
+        ),
+      if (query.isNotEmpty)
+        IconButton(
+          onPressed: () {
+            query = '';
+            _results.clear();
+            showSuggestions(context);
+          },
+          icon: const Icon(Icons.clear),
         )
     ];
   }
@@ -35,52 +56,46 @@ class CustomSearch<T> extends SearchDelegate<T> {
 
   @override
   Widget buildResults(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      children: list
-          .map(
-            (e) => Card(
-              child: ListTile(
-                title: Text(
-                  getTitle(e),
-                ),
-                onTap: () {
-                  onQuery?.call(query);
-                  onResult?.call(e);
-                  close(context, e);
-                },
-              ),
-            ),
-          )
-          .toList(),
-    );
+    return _buildList();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      children: list
-          .where(
-            (element) => getTitle(element).toLowerCase().contains(
-                  query.toLowerCase(),
+    _search(context, query);
+    return _buildList();
+  }
+
+  Widget _buildList() {
+    return FutureBuilder<List<T>>(
+      future: fetchResults(query),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No results found.'));
+        }
+
+        final results = snapshot.data!;
+        return ListView(
+          shrinkWrap: true,
+          children: results
+              .map(
+                (e) => Card(
+                  child: ListTile(
+                    title: Text(getTitle(e)),
+                    onTap: () {
+                      onQuery?.call(query);
+                      onResult?.call(e);
+                      close(context, e);
+                    },
+                  ),
                 ),
-          )
-          .map(
-            (e) => Card(
-              child: ListTile(
-                title: Text(
-                  getTitle(e),
-                ),
-                onTap: () {
-                  onQuery?.call(query);
-                  onResult?.call(e);
-                  close(context, e);
-                },
-              ),
-            ),
-          )
-          .toList(),
+              )
+              .toList(),
+        );
+      },
     );
   }
 }
--- widgets/lib/src/src.dart
+++ widgets/lib/src/src.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:dropdown_search/dropdown_search.dart';
