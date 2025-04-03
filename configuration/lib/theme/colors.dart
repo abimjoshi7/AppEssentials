680b1f5b0d19658dc8e388649dd8a8d311f9892e
abimjoshi7 <abimjoshi7@gmail.com>
2025-04-03 11:23:55 +0545

    chores: color code changes

--- configuration/lib/theme/colors.dart
+++ configuration/lib/theme/colors.dart
@@ -1,12 +1,12 @@
 part of 'theme.dart';
 
 abstract class LightColors {
-  static Color get primary => const Color(0xFF005EB4);
-  static Color get onPrimary => const Color(0xFFFFFFFF);
+  static Color get primary => const Color(0xFF2E587A);
+  static Color get onPrimary => const Color(0xFFF5F5F5);
   static Color get primaryContainer => const Color(0xFFD6E3FF);
   static Color get onPrimaryContainer => const Color(0xFF001B3C);
 
-  static Color get secondary => const Color(0xFF006B57);
+  static Color get secondary => const Color(0xFF62A3BC);
   static Color get onSecondary => const Color(0xFFFFFFFF);
   static Color get secondaryContainer => const Color(0xFF51FCD3);
   static Color get onSecondaryContainer => const Color(0xFF002019);
@@ -25,16 +25,16 @@ abstract class LightColors {
   static Color get onBackground => const Color(0xFF1C1B1F);
   static Color get surface => const Color(0xFFFDFBFF);
   static Color get onSurface => const Color(0xFF1C1B1F);
-  static Color get outline => const Color(0xFF79747E);
+  static Color get outline => const Color(0xFFD6D6D6);
   static Color get surfaceVariant => const Color(0xFFE7E0EC);
-  static Color get onSurfaceVariant => const Color(0xFF49454F);
+  static Color get onSurfaceVariant => const Color(0xFF5A5A5A);
 
   static Color get onInverseSurface => const Color(0xFFECF0FF);
   static Color get inverseSurface => const Color(0xFFECF0FF);
   static Color get inversePrimary => const Color(0xff25A0E2);
   static Color get shadow => const Color(0xFF000000);
   static Color get surfaceTint => const Color(0xFF005EB4);
-  static Color get outlineVariant => const Color(0xFFC4C6CF);
+  static Color get outlineVariant => const Color(0xFFD6D6D6);
   static Color get scrim => const Color(0xFF000000);
 }
 
@@ -45,7 +45,7 @@ abstract class DarkColors {
   static Color get onPrimaryContainer => const Color(0xFFEADDFF);
 
   static Color get secondary => const Color(0xFFCCC2DC);
-  static Color get onSecondary => const Color(0xFF332D41);
+  static Color get onSecondary => const Color(0xFF5A5A5A);
   static Color get secondaryContainer => const Color(0xFF4A4458);
   static Color get onSecondaryContainer => const Color(0xFFE8DEF8);
 
@@ -59,19 +59,29 @@ abstract class DarkColors {
   static Color get errorContainer => const Color(0xFF8C1D18);
   static Color get onErrorContainer => const Color(0xFFF9DEDC);
 
-  static Color get background => const Color(0xFF1C1B1F);
+  static Color get background => const Color(0xFF2B5068);
   static Color get onBackground => const Color(0xFFE6E1E5);
-  static Color get surface => const Color(0xFF1C1B1F);
+  static Color get surface => const Color(0xFF2B5068);
   static Color get onSurface => const Color(0xFFE6E1E5);
-  static Color get outline => const Color(0xFF332D41);
+  static Color get outline => const Color(0xFF5A5A5A);
   static Color get surfaceVariant => const Color(0xFF49454F);
   static Color get onSurfaceVariant => const Color(0xFFCAC4D0);
 
-  static Color get onInverseSurface => const Color(0xff1A1D21);
+  static Color get onInverseSurface => const Color(0xff5A5A5A);
   static Color get inverseSurface => const Color(0xFF1C1B1F);
   static Color get inversePrimary => const Color(0xFF005EB4);
   static Color get shadow => const Color(0xFF000000);
   static Color get surfaceTint => const Color(0xff25A0E2);
-  static Color get outlineVariant => const Color(0xFF49454F);
+  static Color get outlineVariant => const Color(0xFFD6D6D6);
   static Color get scrim => const Color(0xFF000000);
 }
+
+
+// Primary Logo Color : 2E587A
+// Secondary Color: 62A3BC
+// Dark Background : 2B5068
+// Yellow: F4A261
+// Orange: E76F51
+// White text: F5F5F5
+// Grey :D6D6D6
+// Dark Grey : 5A5A5A
